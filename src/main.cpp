b7506fb299f5490919ce474165d62fdcf3d8e7f5
Cihangir Akturk <cihangir.akturk@tubitak.gov.tr>
2025-10-08 11:38:26 +0300

    main: fix command injection vulnerability
    Replace unsafe system() call with QDBusInterface API to prevent
    command injection attacks. The system() call could potentially be
    exploited if an attacker could influence the execution environment.
    
    Using Qt's D-Bus API directly provides type safety and eliminates
    the shell command injection vector.

--- src/main.cpp
+++ src/main.cpp
@@ -35,6 +35,8 @@
 #include <QRect>
 #include <QDesktopWidget>
 #include <QDebug>
+#include <QDBusInterface>
+#include <QDBusReply>
 
 #define SINGLE_INSTANCE ".virtualkeyboard"
 static int setup_unix_signal_handlers();
@@ -76,8 +78,12 @@ int main(int argc, char *argv[])
     {
         if (argc == 2 && QString(argv[1]) == "show") {
             qDebug("Trying to show");
-            return system("qdbus org.eta.virtualkeyboard /VirtualKeyboard "
-                          "org.eta.virtualkeyboard.showForce false");
+            QDBusInterface iface("org.eta.virtualkeyboard",
+                                "/VirtualKeyboard",
+                                "org.eta.virtualkeyboard",
+                                QDBusConnection::sessionBus());
+            QDBusReply<void> reply = iface.call("showForce", false);
+            return reply.isValid() ? 0 : 1;
         } else {
             qDebug("eta-keyboard is allready running");
             return 0;
